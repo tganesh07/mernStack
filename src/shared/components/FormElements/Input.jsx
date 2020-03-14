553ff94d07e744502f54d48a66229def8020dd0a
tganesh07 <tganesh07@gmail.com>
2020-03-14 13:30:44 -0400

    Additional Input added
    Added Descritption as an Input to Add Places Form.

--- src/shared/components/FormElements/Input.jsx
+++ src/shared/components/FormElements/Input.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useEffect } from 'react';
 
 import { validate } from '../../util/validators';
 import './Input.css';
@@ -28,6 +28,14 @@ const Input = props => {
     isTouched: false,
     isValid: false
   });
+
+  // Object de-structuring 
+  const { id, onInput } = props;
+  const { value, isValid } = inputState;
+
+  useEffect(() => {
+    onInput(id, value, isValid);
+  }, [id, value, isValid,oninput]);
 
   const changeHandler = event => {
     dispatch({
